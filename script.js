11efb84907d22b743ef02a87d07486093f717095
R3Dr1Ver <nodiscotd2@gmail.com>
2025-02-11 12:41:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
+    "are you positive?",
+    "please...",
+    "just think about it!",
+    "if you say no, I will be really sad...",
+    "i will be very sad...",
+    "i will be very very very sad...",
     "Ok fine, I will stop asking...",
     "Just kidding, say yes please! ❤️"
 ];
